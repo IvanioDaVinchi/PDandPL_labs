55df7654a00284da9c119501e8a80340a7538797
IvanioDaVinchi <vanyashaplyko@gmail.com>
2020-12-11 11:02:44 +0300

    Add laba13.2 delegate

--- PDandPL_laba13/Delegate/TablePainter.cs
+++ PDandPL_laba13/Delegate/TablePainter.cs
@@ -8,6 +8,56 @@ namespace PDandPL.Delegate
 {
     public class TablePainter
     {
-
+        public TablePainter()
+        {
+            
+        }
+        int a;
+        int b;
+        List<double> listAtoB = new List<double>() { 0,0.57,0.78,1.5, 3.14, 4.71 ,6.28};
+        public double[,] SinTable()
+        {
+            double[,] sinTable = new double[2, listAtoB.Count];
+            for(int i = 0; i < sinTable.GetLength(1); i++)
+            {
+                sinTable[0, i] = listAtoB[i];
+            }
+            for (int j = 0; j < sinTable.GetLength(1); j++)
+            {
+                sinTable[1, j] = Math.Sin(sinTable[0,j]);
+            }
+            return sinTable;
+        }
+        public double[,] SinAndCosTable()
+        {
+            double[,]  Table = new double[2, listAtoB.Count];
+            for (int i = 0; i < Table.GetLength(1); i++)
+            {
+                Table[0, i] = listAtoB[i];
+            }
+            for (int j = 0; j < Table.GetLength(1); j++)
+            {
+                Table[1, j] = Math.Pow(Math.Sin(Table[0, j]), 2) + Math.Pow(Math.Cos(Table[0, j]), 2);
+                
+            }
+            return Table;
+        }
+        public double[,] OneDelX()
+        {
+            double[,] Table = new double[2, listAtoB.Count];
+            for (int i = 0; i < Table.GetLength(1); i++)
+            {
+                Table[0, i] = listAtoB[i];
+            }
+            for (int j = 0; j < Table.GetLength(1); j++)
+            {
+                if(Table[0, j] == 0)
+                {
+                    continue;
+                }
+                Table[1, j] = 1 / Table[0, j];
+            }
+            return Table;
+        }
     }
 }
--- PDandPL_laba13/Program.cs
+++ PDandPL_laba13/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PDandPL.Delegate;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,71 @@ namespace PDandPL
 {
     class Program
     {
+        delegate double[,] Dele();
+        private static void Menu()
+        {
+            TablePainter obj = new TablePainter();
+            Console.WriteLine("Выберите пункт меню:");
+            Console.WriteLine(" 1 - Синус");
+            Console.WriteLine(" 2 - Синус Квадрат Плюс Кос");
+            Console.WriteLine(" 3 - 1/х");
+            int sw = int.Parse(Console.ReadLine());
+            switch (sw)
+            {
+                case 1:
+                    {
+                        Dele dele = new Dele(obj.SinTable);
+                        double[,] table = dele();
+                        Console.WriteLine("Table sin");
+                        Console.WriteLine("0\t30\t45\t90\t180\t270\t360");
+                        for (int i = 1; i < table.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < table.GetLength(1); j++)
+                            {
+                                Console.Write(Math.Round(table[i, j], 2) + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    }
+                case 2:
+                    {
+                        Dele dele = new Dele(obj.SinAndCosTable);
+                        double[,] table2 = dele();
+                        Console.WriteLine("Table sinAndCos ");
+                        Console.WriteLine("0\t30\t45\t90\t180\t270\t360");
+                        for (int i = 1; i < table2.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < table2.GetLength(1); j++)
+                            {
+                                Console.Write(Math.Round(table2[i, j], 2) + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    }
+                case 3:
+                    {
+                        Dele dele = new Dele(obj.OneDelX);
+                        double[,] table3 = dele();
+                        Console.WriteLine("Table 1 / x ");
+                        Console.WriteLine("0\t30\t45\t90\t180\t270\t360");
+                        for (int i = 1; i < table3.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < table3.GetLength(1); j++)
+                            {
+                                Console.Write(Math.Round(table3[i, j], 2) + "\t");
+                            }
+                            Console.WriteLine();
+                        }
+                        break;
+                    }
+            }
+
+        }
         static void Main(string[] args)
         {
-            ArithmeticProgression progression = new ArithmeticProgression(7,20);
-            progression.PrintArithmeticProgression();
-            Console.WriteLine($"Sum progression element = {progression.SumElementsProgression()}");
-            Console.Write("Enter index: ");
-            int index = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{index} element - {progression.GetNElement(index)}");
+            Menu();
             Console.ReadLine();
         }
     }
