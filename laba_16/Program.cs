2eef5a6aeaa9e27ec9ee62e7c1337abcaec66d18
IvanioDaVinchi <vanyashaplyko@gmail.com>
2021-01-22 10:48:36 +0300

    laba 16

--- laba_16/Program.cs
+++ laba_16/Program.cs
@@ -8,15 +8,15 @@ namespace laba_16
 {
     class Program
     {
-        static List<T> FirstTask<T>(List<T> list1, List<T> list2) => list1.Intersect(list2).ToList();
+        static List<T>IntersectionTwoList<T>(List<T> list1, List<T> list2) => list1.Intersect(list2).ToList();
         
-        static void Main(string[] args)
+        static void FirstTask()
         {
             List<int> list1 = new List<int>();
             List<int> list2 = new List<int>();
             Console.WriteLine("Enter count value list1:");
             int count1 = int.Parse(Console.ReadLine());
-            for(int i = 0; i < count1; i++)
+            for (int i = 0; i < count1; i++)
             {
                 Console.WriteLine("Enter value:");
                 list1.Add(int.Parse(Console.ReadLine()));
@@ -29,10 +29,24 @@ namespace laba_16
                 list2.Add(int.Parse(Console.ReadLine()));
             }
             Console.WriteLine("-------");
-            foreach (var item in FirstTask(list1, list2))
+            foreach (var item in IntersectionTwoList(list1, list2))
             {
                 Console.WriteLine(item);
             }
+        }
+        static void SecondTask()
+        {
+            Stack<char> stack = new Stack<char>();
+            stack.Push('A');
+            stack.Push('+');
+            stack.Push('B');
+            stack.Push('*');
+            stack.Push('C');
+            
+        }
+        static void Main(string[] args)
+        {
+            FirstTask();
             Console.ReadLine();
         }
     }
