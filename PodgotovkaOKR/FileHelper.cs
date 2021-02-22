c83b5bc573e430394157a16c535ca90aae950152
IvanioDaVinchi <vanyashaplyko@gmail.com>
2021-02-22 12:39:21 +0300

    biba

---
+++ PodgotovkaOKR/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodgotovkaOKR
+{
+    public class FileHelper
+    {
+        public List<int> ReadFromFile(string path)
+        {
+            List<int> list = new List<int>(); 
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while(reader.EndOfStream)
+                {
+                    list.Add(reader.Read());
+                }
+            }
+            return list;
+        }
+        public void WriteToFile(int digit,string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path,true))
+            {
+                writer.Write(" " + digit);
+            }
+        }
+    }
+}
---
+++ PodgotovkaOKR/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodgotovkaOKR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имена трех файлов");
+            string FirstFile = Console.ReadLine();
+            string SecondFile = Console.ReadLine();
+            string TherdFile = Console.ReadLine();
+            FileHelper helper = new FileHelper();
+            List<int> list = helper.ReadFromFile(FirstFile);
+            if (list.Count % 2 != 0)
+            {
+                int i = 0;
+                while(i != (list.Count() / 2) + 1)
+                {
+
+                }
+            }
+            list.Distinct();
+            list.Sort();
+            
+        }
+    }
+}
