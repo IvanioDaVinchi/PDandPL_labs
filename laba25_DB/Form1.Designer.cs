42410dd37fc52a596c073ea1cf5bc6dcb04352a9
nigvard <vanyashaplyko@gmail.com>
2021-04-01 22:45:50 +0300

    commit

--- laba25_DB/Form1.Designer.cs
+++ laba25_DB/Form1.Designer.cs
@@ -29,39 +29,163 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.иди = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.два = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.три = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.четыре = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 570);
+            this.button1.Location = new System.Drawing.Point(12, 546);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 26);
+            this.button1.Size = new System.Drawing.Size(1083, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "Connection";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
+            // dataGridView3
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(265, 234);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.dataGridView3.Location = new System.Drawing.Point(567, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(529, 528);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.иди,
+            this.два,
+            this.три,
+            this.четыре});
+            this.dataGridView1.Location = new System.Drawing.Point(11, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(550, 262);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 280);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(549, 260);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // иди
+            // 
+            this.иди.HeaderText = "Column1";
+            this.иди.Name = "иди";
+            // 
+            // два
+            // 
+            this.два.HeaderText = "Column1";
+            this.два.Name = "два";
+            // 
+            // три
+            // 
+            this.три.HeaderText = "Column1";
+            this.три.Name = "три";
+            // 
+            // четыре
+            // 
+            this.четыре.HeaderText = "Column1";
+            this.четыре.Name = "четыре";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column1";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column2";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column3";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column4";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Column5";
+            this.Column10.Name = "Column10";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 608);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1107, 603);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,7 +193,23 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn иди;
+        private System.Windows.Forms.DataGridViewTextBoxColumn два;
+        private System.Windows.Forms.DataGridViewTextBoxColumn три;
+        private System.Windows.Forms.DataGridViewTextBoxColumn четыре;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
     }
 }
 
--- laba25_DB/Form1.cs
+++ laba25_DB/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
 
 namespace laba25_DB
 {
@@ -16,10 +18,92 @@ namespace laba25_DB
         {
             InitializeComponent();
         }
+        private void LoadFtable()
+        {
+            using (var connection = new SqliteConnection("Data Source=FlowersShop.db"))
+            {
+                connection.Open();
+                string sqlExpression = "SELECT * FROM Flowers";
 
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                SqliteDataReader reader = command.ExecuteReader();
+                List<string[]> data = new List<string[]>();
+
+                while (reader.Read())
+                {
+                    data.Add(new string[4]);
+
+                    data[data.Count - 1][0] = reader[0].ToString();
+                    data[data.Count - 1][1] = reader[1].ToString();
+                    data[data.Count - 1][2] = reader[2].ToString();
+                    data[data.Count - 1][3] = reader[3].ToString();
+                }
+                foreach (string[] s in data)
+                    dataGridView1.Rows.Add(s);
+
+                reader.Close();
+                connection.Close();
+            }
+        }
+        private void LoadSecTable()
+        {
+            using (var connection = new SqliteConnection("Data Source=FlowersShop.db"))
+            {
+                connection.Open();
+                string sqlExpression = "SELECT * FROM InShop";
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                SqliteDataReader reader = command.ExecuteReader();
+                List<string[]> data = new List<string[]>();
+                while (reader.Read())
+                {
+                    data.Add(new string[4]);
+                    data[data.Count - 1][0] = reader[0].ToString();
+                    data[data.Count - 1][1] = reader[1].ToString();
+                    data[data.Count - 1][2] = reader[2].ToString();
+                    data[data.Count - 1][3] = reader[3].ToString();
+                }
+                foreach (string[] s in data)
+                    dataGridView2.Rows.Add(s);
+
+                reader.Close();
+                connection.Close();
+            }
+        }
+        private void LoadTherdTable()
+        {
+            using (var connection = new SqliteConnection("Data Source=FlowersShop.db"))
+            {
+                connection.Open();
+                string sqlExpression = "SELECT * FROM Sells";
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                SqliteDataReader reader = command.ExecuteReader();
+                List<string[]> data = new List<string[]>();
+                while (reader.Read())
+                {
+                    data.Add(new string[5]);
+                    data[data.Count - 1][0] = reader[0].ToString();
+                    data[data.Count - 1][1] = reader[1].ToString();
+                    data[data.Count - 1][2] = reader[2].ToString();
+                    data[data.Count - 1][3] = reader[3].ToString();
+                    data[data.Count - 1][4] = reader[4].ToString();
+                }
+                foreach (string[] s in data)
+                    dataGridView3.Rows.Add(s);
+
+                reader.Close();
+                connection.Close();
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Image = 
+            LoadFtable();
+            LoadSecTable();
+            LoadTherdTable();
         }
     }
 }
